f2975a5d2b59ca2d57afacae07d734924cc29a5d
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-08-14 15:36:15 +0330

    Bad usage bug was fixed

--- fill.py
+++ fill.py
@@ -2,17 +2,22 @@
 
 from sys import argv
 
-count = argv[1]
-if count[-1] == "B":
-    count = int(count[:-1])
-elif count[-1] == "K":
-    count = int(count[:-1]) * 1024
-elif count[-1] == "M":
-    count = int(count[:-1]) * 1024 * 1024
-elif count[-1] == "G":
-    count = int(count[:-1]) * 1024 * 1024 * 1024
-elif count[-1] == "T":
-    count = int(count[:-1]) * 1024 * 1024 * 1024 * 1024
-else:
-    count = int(count)
+try:
+    count = argv[1]
+    if count[-1] == "B":
+        count = int(count[:-1])
+    elif count[-1] == "K":
+        count = int(count[:-1]) * 1024
+    elif count[-1] == "M":
+        count = int(count[:-1]) * 1024 * 1024
+    elif count[-1] == "G":
+        count = int(count[:-1]) * 1024 * 1024 * 1024
+    elif count[-1] == "T":
+        count = int(count[:-1]) * 1024 * 1024 * 1024 * 1024
+    else:
+        count = int(count)
+except:
+    print("\033[0;31mBad usage\033[0m")
+    print("Who to use? https://github.com/mohammadali-arjomand/fill#usage")
+    exit()
 print("A" * count)
